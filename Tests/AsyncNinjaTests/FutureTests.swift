77ece8f1625911c15198fd5f68f20ed18f4b6c74
Anton Mironov <antonvmironov@gmail.com>
2016-12-23 13:23:56 +0200

    Fixing flaky test

--- Tests/AsyncNinjaTests/FutureTests.swift
+++ Tests/AsyncNinjaTests/FutureTests.swift
@@ -466,8 +466,8 @@ class FutureTests : XCTestCase {
     if let failable = future2D.wait(timeout: timeout) {
       XCTAssertEqual(failable.success!, value)
       let finishTime = DispatchTime.now()
-      XCTAssert(startTime + 0.4 < finishTime)
-      XCTAssert(startTime + 0.6 > finishTime)
+      XCTAssert(startTime + 0.3 < finishTime)
+      XCTAssert(startTime + 0.7 > finishTime)
     } else {
       XCTFail("timeout")
     }
