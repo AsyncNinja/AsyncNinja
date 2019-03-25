7925ff1f415050f08d73429615697efeb2d18661
Loki <sergiy@vynnychenko.com>
2019-03-25 23:34:14 +0200

    promise makers

---
+++ Sources/Promise_makers.swift
@@ -0,0 +1,44 @@
+//
+//  Promise_makers.swift
+//  AsyncNinja
+//
+//  Created by Loki on 3/25/19.
+//
+
+import Foundation
+
+public func promise<T>(
+  executor: Executor = .immediate,
+  after timeout: Double,
+  cancellationToken: CancellationToken?,
+  _ block: @escaping (_ promise: Promise<T>) throws -> Void) -> Promise<T> {
+  let promise = Promise<T>()
+  
+  cancellationToken?.add(cancellable: promise)
+  
+  executor.execute(after: timeout) { [weak promise] (originalExecutor) in
+    if cancellationToken?.isCancelled ?? false {
+      promise?.cancel(from: originalExecutor)
+    } else if let promise = promise {
+      do    { try block(promise) }
+      catch { promise.fail(error) }
+    }
+  }
+  
+  return promise
+}
+
+public func promise<C: ExecutionContext, T>(
+  context: C,
+  executor: Executor? = nil,
+  after timeout: Double,
+  cancellationToken: CancellationToken?,
+  _ block: @escaping (_ promise: Promise<T>) throws -> Void) -> Promise<T> {
+  
+  return promise(executor: executor ?? context.executor,
+                 after: timeout,
+                 cancellationToken: cancellationToken) { promise in
+                  context.addDependent(cancellable: promise)
+                  try block(promise)
+  }
+}
