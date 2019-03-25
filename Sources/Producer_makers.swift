10886be59cd0be5440de09303690f0c0fe45df70
Loki <sergiy@vynnychenko.com>
2019-03-25 23:34:31 +0200

    indentation fixes

--- Sources/Producer_makers.swift
+++ Sources/Producer_makers.swift
@@ -11,41 +11,41 @@ import Dispatch
 /// Convenience constructor of Producer
 /// Gives an access to an underlying Producer to a provided block
 public func producer<Update, Success>(
-    executor: Executor = .immediate,
-    cancellationToken: CancellationToken? = nil,
-    bufferSize: Int = AsyncNinjaConstants.defaultChannelBufferSize,
-    block: @escaping (_ producer: Producer<Update, Success>) throws -> Void
-    ) -> Producer<Update, Success> {
-    // TEST: ChannelMakersTests.testMakeChannelWithProducerProvidingBlock
-    
-    let producer = Producer<Update, Success>(bufferSize: bufferSize)
-    cancellationToken?.add(cancellable: producer)
-    executor.execute(from: nil) { (originalExecutor) in
-        do {
-            try block(producer)
-        } catch {
-            producer.fail(error, from: originalExecutor)
-        }
+  executor: Executor = .immediate,
+  cancellationToken: CancellationToken? = nil,
+  bufferSize: Int = AsyncNinjaConstants.defaultChannelBufferSize,
+  block: @escaping (_ producer: Producer<Update, Success>) throws -> Void
+  ) -> Producer<Update, Success> {
+  // TEST: ChannelMakersTests.testMakeChannelWithProducerProvidingBlock
+  
+  let producer = Producer<Update, Success>(bufferSize: bufferSize)
+  cancellationToken?.add(cancellable: producer)
+  executor.execute(from: nil) { (originalExecutor) in
+    do {
+      try block(producer)
+    } catch {
+      producer.fail(error, from: originalExecutor)
     }
-    return producer
+  }
+  return producer
 }
 
 /// Convenience constructor of Producer
 /// Gives an access to an underlying Producer to a provided block
 public func producer<C: ExecutionContext, Update, Success>(
-    context: C,
-    executor: Executor? = nil,
-    cancellationToken: CancellationToken? = nil,
-    bufferSize: Int = AsyncNinjaConstants.defaultChannelBufferSize,
-    block: @escaping (_ context: C, _ strongProducer: Producer<Update, Success>) throws -> Void
-    ) -> Producer<Update, Success> {
-    // TEST: ChannelMakersTests.testMakeChannelWithProducerProvidingBlockWithDeadContext
-    
-    return producer(executor: executor ?? context.executor,
-                   cancellationToken: cancellationToken,
-                   bufferSize: bufferSize
-    ) { (producer: Producer<Update, Success>) in
-        context.addDependent(completable: producer)
-        try block(context, producer)
-    }
+  context: C,
+  executor: Executor? = nil,
+  cancellationToken: CancellationToken? = nil,
+  bufferSize: Int = AsyncNinjaConstants.defaultChannelBufferSize,
+  block: @escaping (_ context: C, _ strongProducer: Producer<Update, Success>) throws -> Void
+  ) -> Producer<Update, Success> {
+  // TEST: ChannelMakersTests.testMakeChannelWithProducerProvidingBlockWithDeadContext
+  
+  return producer(executor: executor ?? context.executor,
+                  cancellationToken: cancellationToken,
+                  bufferSize: bufferSize
+  ) { (producer: Producer<Update, Success>) in
+    context.addDependent(completable: producer)
+    try block(context, producer)
+  }
 }
