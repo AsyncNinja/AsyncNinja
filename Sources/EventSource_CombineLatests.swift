ec4fdc0385a7ef42782effe890521dad4f0cab03
Loki <sergiy@vynnychenko.com>
2019-05-24 11:17:16 +0300

    CombineLatest is extension of ExecutionContext

--- Sources/EventSource_CombineLatests.swift
+++ Sources/EventSource_CombineLatests.swift
@@ -7,52 +7,59 @@
 
 import Foundation
 
-typealias ES = EventSource
+public typealias ES = EventSource
 
-func combineLatestUpdates<ES1:ES, ES2:ES>(_ es1: ES1, _ es2: ES2, executor: Executor = Executor.main)
-  -> Channel<(ES1.Update,ES2.Update),(ES1.Success, ES2.Success)> {
-  let combine = CombineLatest2(es1, es2, executor: executor)
-  
-  combine.producer._asyncNinja_retainHandlerUntilFinalization(combine)
-  combine.producer._asyncNinja_notifyFinalization { print("combineLatestUpdates Finalization") }
-  
-  return combine.producer
+public extension ExecutionContext {
+  func combineLatest<ES1:ES, ES2:ES>(_ es1: ES1, _ es2: ES2, executor: Executor? = nil)
+    -> Channel<(ES1.Update,ES2.Update),(ES1.Success, ES2.Success)> {
+      
+      return CombineLatest2(es1, es2, executor: executor ?? self.executor)
+        .retain(with: self)
+        .producer
+  }
 }
 
-fileprivate class CombineLatest2<ES1:ES, ES2:ES> : ExecutionContext, ReleasePoolOwner {
-  typealias ResultUpdate  = (ES1.Update,  ES2.Update)
-  typealias ResultSuccess = (ES1.Success, ES2.Success)
-  
+fileprivate class RetainableProducerOwner<Update,Success> : ExecutionContext, ReleasePoolOwner {
   public let releasePool = ReleasePool()
   public var executor: Executor
+  var producer = Producer<Update,Success>()
   
+  
+  init(executor: Executor) {
+    self.executor = executor
+  }
+  
+  func retain(with releasePool: Retainer) -> RetainableProducerOwner {
+    releasePool.releaseOnDeinit(self)
+    return self
+  }
+}
+
+
+fileprivate class CombineLatest2<ES1:ES, ES2:ES> : RetainableProducerOwner<(ES1.Update, ES2.Update), (ES1.Success, ES2.Success)> {
   var update1 : ES1.Update?
   var update2 : ES2.Update?
   
   var success1 : ES1.Success?
   var success2 : ES2.Success?
   
-  let producer = Producer<ResultUpdate,ResultSuccess>()
-  
   init(_ es1: ES1, _ es2: ES2, executor: Executor) {
-    self.executor = executor
-    
-    producer._asyncNinja_retainUntilFinalization(self)
+    super.init(executor: executor)
     
     es1.onUpdate(context: self) { ctx, upd in     ctx.update1 = upd;      ctx.trySendUpdate() }
       .onSuccess(context: self) { ctx, success in ctx.success1 = success; ctx.tryComplete()   }
-      .onFailure(context: self) { ctx, error in   ctx.producer.fail(error) }
+      .onFailure(context: self) { ctx, error in   ctx.producer.fail(error, from: executor) }
     
     es2.onUpdate(context: self) { ctx, upd in     ctx.update2 = upd;      ctx.trySendUpdate() }
       .onSuccess(context: self) { ctx, success in ctx.success2 = success; ctx.tryComplete()   }
-      .onFailure(context: self) { ctx, error in   ctx.producer.fail(error) }
+      .onFailure(context: self) { ctx, error in   ctx.producer.fail(error, from: executor) }
   }
   
   func trySendUpdate() {
     guard let upd1 = update1 else { return }
     guard let upd2 = update2 else { return }
     
-    producer.update((upd1,upd2))
+    producer.update((upd1,upd2), from: executor)
   }
   
   func tryComplete() {
